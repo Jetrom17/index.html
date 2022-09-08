aa3a96c33fdde9a18b3b86c970fda0825e91cf31
Jeiel <Jeiel17@protonmail.com>
2022-09-08 08:50:33 -0300

    Create index3.js

---
+++ index3.js
@@ -0,0 +1,14 @@
+[
+  {
+    "image": "true",
+    "imageURL": "https://logos-marques.com/wp-content/uploads/2021/03/Marvel-Logo-2048x1158.png",
+    "Title": "Seu app de séries",
+    "Message": "https://streamtape.com/e/VObyeg2dm2fKW9Q/"
+  },
+  {
+    "image": "false",
+    "imageURL": "",
+    "Title": "Assista agora",
+    "Message": "Episódio 4 - dublado"
+  }
+]
