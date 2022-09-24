90d47e4072fafac41234a4d30943bf79db6b209a
Jeiel <Jeiel17@protonmail.com>
2022-09-24 20:29:14 -0300

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,20 +1,8 @@
 [
   {
-    "image": "true",
-    "imageURL": "https://logos-marques.com/wp-content/uploads/2021/03/Marvel-Logo-2048x1158.png",
-    "Title": "Seu app de séries",
-    "Message": "https://streamtape.com/e/VObyeg2dm2fKW9Q/"
-  },
-  {
     "image": "false",
     "imageURL": "",
-    "Title": "Assista agora",
-    "Message": "Episódio 4 - dublado"
+    "Title": "Versão 1.12.",
+    "Message": "Primeiro registro da versão. Novidades sobre essa versão está indisponível. Confira versão posterior."
   },
-{
-    "image": "false",
-    "imageURL": "",
-    "Title": "Episódio 5 às 16: horas aproximadamente",
-    "Message": "https://embed.warezcdn.net/serie/tt10857160"
-}
 ]
