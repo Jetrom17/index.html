b83d8a91a2b7ca3b54f7b339d4bd836a89b7830c
Jeiel <Jeiel17@protonmail.com>
2022-10-03 17:18:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 {
   "Visibility": "VISIBLE",
   "Title": "Atualização",
-  "Description": "Atualize para versão mais recente",
+  "Description": "Atualize para versão mais recente e explore novidades!",
   "MainTextButton": "Update",
   "Cancelable": "true",
   "CancelButtonText": "Cancel",
-  "DownloadLink": "https://www.mediafire.com/file/ucuqk7nq4ckvqcj/Marvel_Flix_3.0.apk/file",
-  "Version": "1.3"
+  "DownloadLink": "https://www.mediafire.com/file/mv4o7fp7gsqvxwx/Marvel_Flix_1.4.apk/file",
+  "Version": "1.4"
 }
