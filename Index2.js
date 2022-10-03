c4991577a7b611e8f1939dc0d5f77ca6009cc073
Jeiel <Jeiel17@protonmail.com>
2022-10-03 17:25:54 -0300

    Update Index2.js

--- Index2.js
+++ Index2.js
@@ -3,7 +3,7 @@
     "image": "true",
     "imageURL": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1TIXfDRMAFXkjqBm4VN0bAHaEF%26pid%3DApi&f=1&ipt=94b6dc6666281a79cede9046809a2602ad78f08d5103db69c9190eff75c13043&ipo=images",
     "Title": "Acesse pelo seu navegador",
-    "Message": "https://jetrom-e31b4.web.app/"
+    "Message": "https://jetrom.autocode.dev/temp@dev/temp.html"
   },
   {
     "image": "false",
