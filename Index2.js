bf77eea4ef70333c7720dd216f16e2bc37ea2a38
Jeiel <Jeiel17@protonmail.com>
2022-10-03 17:24:44 -0300

    Update Index2.js

--- Index2.js
+++ Index2.js
@@ -1,20 +1,20 @@
 [
   {
     "image": "true",
-    "imageURL": "https://logos-marques.com/wp-content/uploads/2021/03/Marvel-Logo-2048x1158.png",
-    "Title": "Seu app de séries",
-    "Message": "https://streamtape.com/e/VObyeg2dm2fKW9Q/"
+    "imageURL": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1TIXfDRMAFXkjqBm4VN0bAHaEF%26pid%3DApi&f=1&ipt=94b6dc6666281a79cede9046809a2602ad78f08d5103db69c9190eff75c13043&ipo=images",
+    "Title": "Acesse pelo seu navegador",
+    "Message": "https://jetrom-e31b4.web.app/"
   },
   {
     "image": "false",
     "imageURL": "",
-    "Title": "Assista agora",
-    "Message": "Episódio 4 - dublado"
+    "Title": "Telegram",
+    "Message": "Siga nós: https://t.me/sandbox_enigma"
   },
 {
     "image": "false",
     "imageURL": "",
-    "Title": "Aguarde",
-    "Message": "Episódio 5 será colocado assim no site também!"
+    "Title": "Novidades da versão 1.4",
+    "Message": "Possibilidade de assistir em tela cheia. Alteração dos botões - Alternativa -"
   }
 ]
