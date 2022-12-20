bcee0449e1a72d90cdcbfe9babb76ef3aead4d36
Jeiel <Jeiel17@protonmail.com>
2022-12-19 22:03:05 -0300

    Update Index2.js

--- Index2.js
+++ Index2.js
@@ -3,13 +3,7 @@
     "image": "true",
     "imageURL": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1TIXfDRMAFXkjqBm4VN0bAHaEF%26pid%3DApi&f=1&ipt=94b6dc6666281a79cede9046809a2602ad78f08d5103db69c9190eff75c13043&ipo=images",
     "Title": "Acesse pelo seu navegador",
-    "Message": "https://jetrom.autocode.dev/temp@dev/temp.html"
-  },
-  {
-    "image": "false",
-    "imageURL": "",
-    "Title": "Telegram",
-    "Message": "Siga nós: https://t.me/sandbox_enigma"
+    "Message": "https://jetronix.tk"
   },
 {
     "image": "false",
@@ -18,10 +12,10 @@
     "Message": "Possibilidade de assistir em tela cheia. Alteração dos botões - Alternativa -"
   },
   {
-    "image": "false",
-    "imageURL": "",
-    "Title": "Outras séries recentes",
-    "Message": "https://jetrom-e31b4.web.app/"
+    "image": "true",
+    "imageURL": "https://i.imgur.com/0S9qgAn_d.webp?maxwidth=640&shape=thumb&fidelity=medium",
+    "Title": "Flor de Lis",
+    "Message": "https://baixarseriesmp4.xyz/baixar-flordelis-em-nome-da-mae-1a-temporada-mp4-nacional/#more-113211"
   },
   {
     "image": "true",
